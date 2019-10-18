7fec36ddf0961a013c87fb4205dd34267da73dd8
Rebecca Hong <Rebeccahongsf@gmail.com>
2019-10-18 14:31:54 -0700

    Update Boxen, Add styling

--- index.js
+++ index.js
@@ -4,19 +4,19 @@ const boxen = require('boxen');
 const chalk = require('chalk');
 
 const card = boxen(`
-${chalk.bold('Rebecca Hong')} | Front End Developer
-${chalk.bold('-'.repeat(40))}
+${chalk.green.bold('Rebecca Hong')} | Front End Developer
+${chalk.green.bold('-'.repeat(40))}
 
-${chalk.bold('Github:')} https://github.com/rebeccahongsf
-${chalk.bold('LinkedIn:')} https://linkedin.com/in/rehong
-${chalk.bold('NPM:')} https://www.npmjs.com/~rebeccahong
-${chalk.bold('Portfolio:')} http://rebeccahong.me
-${chalk.bold('Instagram:')} https://instagram.com/doodledeveloper
+${chalk.green.bold('Github:')} https://github.com/rebeccahongsf
+${chalk.green.bold('LinkedIn:')} https://linkedin.com/in/rehong
+${chalk.green.bold('NPM:')} https://www.npmjs.com/~rebeccahong
+${chalk.green.bold('Portfolio:')} http://rebeccahong.me
+${chalk.green.bold('Instagram:')} https://instagram.com/doodledeveloper
 `,
     {
-        borderColor: 'cyan',
-        borderStyle: 'double',
-        padding: 1,
+        borderColor: '#00a7a7',
+        borderStyle: 'bold',
+        padding: 1
     }
 );
 
